c491f48b589bbf993b13ba1554d03e5a498e95e9
my-name <my-email>
2025-03-31 16:08:52 +0100

    Update: Also add another elif in a backward compatible way so we dont break existing users nor create any Git conflicts.
    Signed-off-by: my-name <my-email>

--- wait-for-plugins.sh
+++ wait-for-plugins.sh
@@ -17,6 +17,9 @@ EXTRA_CLI_ARGS=""
 if [ -f "$USER_APP_CONFIG" ]; then
     echo "Using user app-config.local.yaml"
     EXTRA_CLI_ARGS="--config $USER_APP_CONFIG"
+elif [ -f "configs/app-config.local.yaml" ]; then
+    echo "[warn] Using legacy app-config.local.yaml. This method is deprecated. You should move your local app-config file under configs/app-config/app-config.local.yaml and extra files under configs/extra-files."
+    EXTRA_CLI_ARGS="--config configs/app-config.local.yaml"
 fi
 
 # Run Backstage with default + optional config overrides
